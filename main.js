a6184b814ebbd6fa9cb2396ecd3c453f43dd89ac
Andrzej Wódkiewicz <akwodkiewicz@gmail.com>
2018-08-07 00:01:59 +0200

    Denodify project

--- main.js
+++
@@ -1,9 +0,0 @@
-var connect = require("connect");
-var serveStatic = require("serve-static");
-
-var app = connect();
-
-app.use(serveStatic("app"));
-app.listen(5000, () =>
-  console.log("Server is now listening on http://localhost:5000")
-);
