390fe8e01474cc8c28804f74bfc5ae229e55d23f
Andrzej Wódkiewicz <akwodkiewicz@gmail.com>
2018-08-07 16:50:41 +0200

    Add some info to html; add css; resize canvas

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var myp5 = new p5(function(sketch) {
   var car, frontWheel, rearWheel, geometry;
 
   sketch.setup = function() {
-    sketch.createCanvas(800, 800);
+    sketch.createCanvas(1000, 800);
     sketch.angleMode(sketch.RADIANS);
     sketch.ellipseMode(sketch.RADIUS);
     sketch.rectMode(sketch.CENTER);
